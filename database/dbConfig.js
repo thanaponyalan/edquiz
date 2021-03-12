81db74ccde3ad72928bb525166f1a29807962bea
Thanapon Yalan <thanaponyalan@gmail.com>
2021-03-12 13:59:47 +0700

    mongoose

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -4,7 +4,7 @@ const mongoose=require('mongoose');
 const {Schema}=mongoose;
 
 mongoose.Promise=global.Promise
-mongoose.connect(MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
+mongoose.connect(MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true}).then(()=>{
     console.log("[success] task 2 : connected to the database ");
 },error=>{
     console.log("[failed] task 2 : "+error);
