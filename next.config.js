0e335c5572d3dcf86a6aac30a2fe76c9195b9c12
Thanapon Yalan <thanaponyalan@gmail.com>
2021-03-14 13:18:48 +0700

    config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        API_SERVER: process.env.API_SERVER,
+        API_SERVER: process.env.API_SERVER||'http://localhost:3000',
         MONGO_URI: process.env.MONGO_URI,
         GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
         GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET
